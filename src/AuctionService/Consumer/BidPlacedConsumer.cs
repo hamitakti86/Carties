14482a4a63840230ee7f178aa2f2e2f1a88ed9e5
hamitakti86 <37433210+hamitakti86@users.noreply.github.com>
2024-02-21 02:06:40 +0300

    Added Gprc Into Bid Service

--- src/AuctionService/Consumer/BidPlacedConsumer.cs
+++ src/AuctionService/Consumer/BidPlacedConsumer.cs
@@ -17,10 +17,10 @@ public class BidPlacedConsumer : IConsumer<BidPlaced>
     {
         Console.WriteLine("--> Consuming bid placed");
 
-        var auction = await _dbContext.Auctions.FindAsync(context.Message.AuctionId);
+        var auction = await _dbContext.Auctions.FindAsync(Guid.Parse(context.Message.AuctionId));
 
-        if (auction.CurrentHighBid == null 
-            || context.Message.BidStatus.Contains("Accepted") 
+        if (auction.CurrentHighBid == null
+            || context.Message.BidStatus.Contains("Accepted")
             && context.Message.Amount > auction.CurrentHighBid)
         {
             auction.CurrentHighBid = context.Message.Amount;
